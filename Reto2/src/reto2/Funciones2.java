46e3a1b0034ba06cefb840214122e32786d6cc1e
alvarezleiva <galvarezleiv@gmail.com>
2024-10-31 08:55:11 +0100

    Creados submenús fahrenheit_celsius y celsius_fahrenheit

--- Reto2/src/reto2/Funciones2.java
+++ Reto2/src/reto2/Funciones2.java
@@ -1,5 +1,14 @@
 package reto2;
 
 public class Funciones2 {
-
+	
+	public static double Fahrenheit_Celsius(double num1){
+		
+		return (num1 - 32) * 5/9;
+		
+	}
+	
+	public static double Celsius_Fahrenheit (double num1) {
+		return (num1 * 9/5) + 32;
+	}
 }
--- Reto2/src/reto2/Reto2.java
+++ Reto2/src/reto2/Reto2.java
@@ -1,10 +1,40 @@
 package reto2;
 
+import java.util.Scanner;
+
+import Reto2.Funciones;
+
 public class Reto2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = New Scanner(System.in);
+		do {
+			// Funcion imprimir para poner lineas por consola
+			System.out.println("----");
+			System.out.println("Elige la operacion");
+			System.out.println("----");
+			System.out.println("1. Longitud");
+			System.out.println("2. Peso");
+			System.out.println("3. Temperatura");
+			System.out.println("4. Volumen");
+			System.out.println("0. Salir");
+
+			// Validacion del numero introducido
+			do {
+
+				int opcion = Funciones.dimeEntero("			System.out.println(\"----\");\r\n"
+						+ "			System.out.println(\"Elige la operacion\");\r\n"
+						+ "			System.out.println(\"----\");\r\n"
+						+ "			System.out.println(\"1. Longitud\");\r\n"
+						+ "			System.out.println(\"2. Peso\");\r\n"
+						+ "			System.out.println(\"3. Temperatura\");\r\n"
+						+ "			System.out.println(\"4. Volumen\");\r\n"
+						+ "			System.out.println(\"0. Salir\");" , scan);
+			} while (true);
+
+	
+
 
 	}
-
+}
 }
