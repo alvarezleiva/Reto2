8c17432f03a4ccc6e8769ccd8403b6ca643cf6bf
PzDarby <izliquihd@gmail.com>
2024-10-31 08:44:45 +0100

    He añadido la funcion del submenu

--- Reto2/src/reto2/Funciones1.java
+++ Reto2/src/reto2/Funciones1.java
@@ -1,5 +1,23 @@
 package reto2;
 
+import java.util.Scanner;
+
 public class Funciones1 {
 
+	public static int subMenu(String tipoConversion,String texto1, String texto2,Scanner sc) {
+		do {
+			System.out.println("Selecciona la conversion de "+tipoConversion);
+			System.out.println(texto1);
+			System.out.println(texto2);
+			int num=Funciones.dimeEntero("",sc);
+			if(num==1)
+				return num;
+			else if(num==2)
+				return num;
+			else if(num==0)
+				return 0;
+			else
+				System.out.println("Debe ser una de las opciones indicadas");
+		} while (true);
+	}
 }
