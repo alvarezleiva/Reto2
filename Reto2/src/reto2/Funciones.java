313611c66065abb94b9ffe767518e482b9453af7
alvarezleiva <galvarezleiv@gmail.com>
2024-10-31 08:21:10 +0100

    Creación de proyecto Reto2, y clases funciones, funciones1, funciones2

---
+++ Reto2/src/reto2/Funciones.java
@@ -0,0 +1,62 @@
+package reto2;
+
+import java.util.Scanner;
+
+public class Funciones {
+
+	public static void main(String[] args) {
+
+		
+		Scanner sc = new Scanner(System.in);
+
+		int ent = dimeEntero("Introduce un número entero", sc);
+		double dou = dimeDouble("Introduce un número decimal", sc);
+	}
+
+	public static boolean esInt(String s) {
+
+		try {
+			int num = Integer.parseInt(s);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public static boolean esDouble(String s) {
+		try {
+			double num = Integer.parseInt(s);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public static int dimeEntero(String texto, Scanner sc) {
+		do {
+			try {
+				System.out.println(texto);
+				String s = sc.nextLine();
+				int num = Integer.parseInt(s);
+				return num;
+			} catch (Exception ex) {
+				System.out.println("Introduce un nº correcto");
+			}
+		} while (true);
+
+	}
+	public static double dimeDouble(String texto, Scanner sc) {
+		do {
+			try {
+				System.out.println(texto);
+				String s = sc.nextLine();
+				double num2 = Double.parseDouble(s);
+				return num2;
+			} catch (Exception ex) {
+				System.out.println("Introduce un nº decimal correcto");
+			}
+		} while (true);
+
+	}
+
+}
---
+++ Reto2/src/reto2/Reto2.java
@@ -0,0 +1,10 @@
+package reto2;
+
+public class Reto2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
