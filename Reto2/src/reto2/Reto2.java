9c51eb4315abad65aa2f639d6fcbddf531ab669e
PzDarby <izliquihd@gmail.com>
2024-11-04 12:26:27 +0100

    Menu simplificado

--- Reto2/src/reto2/Reto2.java
+++ Reto2/src/reto2/Reto2.java
@@ -12,12 +12,7 @@ public class Reto2 {
 			System.out.println("----");
 			System.out.println("Elige la operacion");
 			System.out.println("----");
-			System.out.println("1. Longitud");
-			System.out.println("2. Peso");
-			System.out.println("3. Temperatura");
-			System.out.println("4. Volumen");
-			System.out.println("0. Salir");
-
+			System.out.println("1. Longitud\n2. Peso\n3. Temperatura\n4. Volumen\n0. Salir");
 			// Validacion del numero introducido
 
 			opcion = Funciones.dimeEntero("", sc);
@@ -28,7 +23,7 @@ public class Reto2 {
 			    int opcionlongitud = Funciones1.subMenu("Longitud", "Millas a Kilometros", "Kilometros a millas", sc);
 			    if(opcionlongitud == 1){
 			    	double longitud = Funciones.dimeDouble("Dime millas", sc);
-			    	System.out.println("El resultado es " + Funciones1.conversionDeMillasKm(opcion));
+			    	System.out.println("El resultado es " + Funciones1.conversionDeMillasKm(longitud));
 			    }
 			    break;
 	
